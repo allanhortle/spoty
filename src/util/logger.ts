ed23ae05c90353870e903715cf67803d8a796058
Allan Hortle <allanhortle@gmail.com>
2024-02-08 20:25:17 +1100

    Upgrade search to react-curse

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,9 +1,9 @@
 import winston from 'winston';
 import path from 'path';
-import {logPath} from './storage';
+import {logPath} from './storage.js';
 
 export default winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(winston.format.errors({stack: true}), winston.format.json()),
     transports: [new winston.transports.File({filename: path.join(logPath, 'spoty.log')})]
 });
